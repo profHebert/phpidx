96fa3e6e66caa0bf1c24c1c3767c743fc9ccf850
Hebert Bratefixe Alquimim <hebert@uni9.pro.br>
2026-04-29 10:31:07 -0300

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,38 +1,33 @@
 { pkgs, ... }: {
   channel = "stable-23.11";
+
   packages = [
     pkgs.php82
+    pkgs.php82Packages.composer
     pkgs.mysql80
     pkgs.php82Extensions.mysqli
     pkgs.php82Extensions.pdo_mysql
-    pkgs.curl
-    pkgs.gnutar
-    pkgs.gzip
   ];
 
-  services.mysql.enable = true;
+  # Ativa o MySQL automaticamente
+  services.mysql = {
+    enable = true;
+    package = pkgs.mysql80;
+  };
 
   idx = {
-    extensions = [ "bmewburn.vscode-intelephense-client" ];
-    workspace.onCreate = {
-      # Cria a pasta do site e baixa o phpMyAdmin automaticamente
-      setup-ambiente = ''
-        mkdir -p www
-        if [ ! -d "phpmyadmin" ]; then
-          mkdir phpmyadmin
-          curl -L https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.tar.gz | tar xz -C phpmyadmin --strip-components=1
-        fi
-      '';
-    };
+    extensions = [
+      "bmewburn.vscode-intelephense-client"
+      "mynameless.php-extension-pack"
+    ];
+
+    # ESSA É A PARTE QUE O ERRO DIZ QUE ESTÁ FALTANDO:
     previews = {
       enable = true;
       previews = {
         web = {
-          command = ["php" "-S" "0.0.0.0:8080" "-t" "www"];
-          manager = "web";
-        };
-        pma = {
-          command = ["php" "-S" "0.0.0.0:8081" "-t" "phpmyadmin"];
+          # Se o seu index.php estiver na raiz, mude "-t public" para "-t ."
+          command = ["php" "-S" "0.0.0.0:$PORT" "-t" "public"];
           manager = "web";
         };
       };
